d7c9cad41160352c258dbb82b00d31f907a9e092
Lucas Soares <lucas.soares02@hotmail.com>
2022-02-14 20:44:18 -0300

    updated method get highliths

--- src/controller/Cookbook.js
+++ src/controller/Cookbook.js
@@ -60,6 +60,22 @@ const Cookbook = {
       if (error) {
         console.log("Error Select Users: ", error);
       } else {
+        results.forEach((element, index) => {
+          results[index]["title"] = element["title"].replace("\n", "");
+
+          results[index]["ingredients"] = element["ingredients"].split("\n");
+
+          results[index]["preparation"] =
+            element["preparationMode"].split("\n");
+
+          results[index]["preparation"].shift();
+          results[index]["ingredients"].shift();
+
+          results[index]["emphasis"] = index % 2 == 0 ? false : true;
+          results[index]["like"] = index % 2 == 0 ? true : false;
+          results[index]["chefe"] = "Fernadez Gonçalvez";
+        });
+
         return res.json(results);
       }
     });
